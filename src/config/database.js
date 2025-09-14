60901a04199f79a2d236f72deab2891da40fd377
Abdul Rafay <rafay0704@gmail.com>
2025-09-15 03:50:42 +0400

    db config

--- src/config/database.js
+++ src/config/database.js
@@ -12,4 +12,9 @@ if (process.env.NODE_ENV === 'development') {
 const sql = neon(process.env.DATABASE_URL);
 const db = drizzle(sql);
 
+
+
 export { db, sql };
+
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
